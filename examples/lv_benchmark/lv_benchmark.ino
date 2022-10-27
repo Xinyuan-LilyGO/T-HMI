0ddcf98c7d2536b3e74730c9087a00a2e9305b40
liangyingy <1102390310@qq.com>
2022-10-27 17:48:25 +0800

    feature: add lv_benchmark example
    Signed-off-by: liangyingy <1102390310@qq.com>

---
+++ examples/lv_benchmark/lv_benchmark.ino
@@ -0,0 +1,220 @@
+#include "image/logo.h"
+#include "init_code.h"
+
+#include "benchmark/lv_demo_benchmark.h"
+
+#include "lv_conf.h"
+#include "lvgl.h"
+
+#include "esp_lcd_panel_io.h"
+#include "esp_lcd_panel_ops.h"
+#include "esp_lcd_panel_rgb.h"
+#include "esp_lcd_panel_vendor.h"
+
+#include <Wire.h>
+#include <Arduino.h>
+
+/**
+ * Please update the following configuration according to your LCD spec
+ */
+#define EXAMPLE_LCD_PIXEL_CLOCK_HZ     (5 * 1000 * 1000)
+#define EXAMPLE_LCD_BK_LIGHT_ON_LEVEL  (1)
+#define EXAMPLE_LCD_BK_LIGHT_OFF_LEVEL (!EXAMPLE_LCD_BK_LIGHT_ON_LEVEL)
+#define EXAMPLE_PIN_NUM_DATA0          (48)
+#define EXAMPLE_PIN_NUM_DATA1          (47)
+#define EXAMPLE_PIN_NUM_DATA2          (39)
+#define EXAMPLE_PIN_NUM_DATA3          (40)
+#define EXAMPLE_PIN_NUM_DATA4          (41)
+#define EXAMPLE_PIN_NUM_DATA5          (42)
+#define EXAMPLE_PIN_NUM_DATA6          (45)
+#define EXAMPLE_PIN_NUM_DATA7          (46)
+#define EXAMPLE_PIN_NUM_PCLK           (8)
+#define EXAMPLE_PIN_NUM_CS             (6)
+#define EXAMPLE_PIN_NUM_DC             (7)
+#define EXAMPLE_PIN_NUM_RST            (-1)
+#define EXAMPLE_PIN_NUM_BK_LIGHT       (38)
+
+/**
+ * The pixel number in horizontal and vertical
+ */
+#define EXAMPLE_LCD_H_RES              (240)
+#define EXAMPLE_LCD_V_RES              (320)
+
+/**
+ * Bit number used to represent command and parameter
+ */
+#define EXAMPLE_LCD_CMD_BITS           (8)
+#define EXAMPLE_LCD_PARAM_BITS         (8)
+
+
+static bool touch_pin_get_int = false;
+
+
+void scan_iic(void) {
+    byte error, address;
+    int nDevices = 0;
+    Serial.println("Scanning for I2C devices ...");
+    for (address = 0x01; address < 0x7f; address++) {
+        Wire.beginTransmission(address);
+        error = Wire.endTransmission();
+        if (error == 0) {
+            Serial.printf("I2C device found at address 0x%02X\n", address);
+            nDevices++;
+        } else if (error != 2) {
+            Serial.printf("Error %d at address 0x%02X\n", error, address);
+        }
+    }
+    if (nDevices == 0) {
+        Serial.println("No I2C devices found");
+    }
+}
+
+
+void print_chip_info(void) {
+    Serial.print("Chip: ");
+    Serial.println(ESP.getChipModel());
+    Serial.print("ChipRevision: ");
+    Serial.println(ESP.getChipRevision());
+    Serial.print("Psram size: ");
+    Serial.print(ESP.getPsramSize() / 1024);
+    Serial.println("KB");
+    Serial.print("Flash size: ");
+    Serial.print(ESP.getFlashChipSize() / 1024);
+    Serial.println("KB");
+    Serial.print("CPU frequency: ");
+    Serial.print(ESP.getCpuFreqMHz());
+    Serial.println("MHz");
+}
+
+
+static void example_lvgl_flush_cb(lv_disp_drv_t *drv, const lv_area_t *area, lv_color_t *color_map) {
+    esp_lcd_panel_handle_t panel_handle = (esp_lcd_panel_handle_t)drv->user_data;
+    int offsetx1 = area->x1;
+    int offsetx2 = area->x2;
+    int offsety1 = area->y1;
+    int offsety2 = area->y2;
+    esp_lcd_panel_draw_bitmap(panel_handle, offsetx1, offsety1, offsetx2 + 1, offsety2 + 1, color_map);
+    lv_disp_flush_ready(drv);
+}
+
+
+// static void lv_touchpad_read(lv_indev_drv_t *indev_driver, lv_indev_data_t *data) {
+//     uint8_t touch_points_num;
+//     uint16_t x, y;
+//     if (touch_pin_get_int) {
+//         ft3267_read_pos(&touch_points_num, &x, &y);
+//         data->state = LV_INDEV_STATE_PR;
+//         data->point.x = x;
+//         data->point.y = y;
+//         touch_pin_get_int = false;
+//     } else {
+//         data->state = LV_INDEV_STATE_REL;
+//     }
+// }
+
+
+void setup() {
+    static lv_disp_draw_buf_t disp_buf; // contains internal graphic buffer(s) called draw buffer(s)
+    static lv_disp_drv_t disp_drv;      // contains callback functions
+    static lv_indev_drv_t indev_drv;
+
+    Serial.begin(115200);
+
+    print_chip_info();
+    pinMode(10, OUTPUT);
+    digitalWrite(10, HIGH);
+    pinMode(EXAMPLE_PIN_NUM_BK_LIGHT, OUTPUT);
+    digitalWrite(EXAMPLE_PIN_NUM_BK_LIGHT, EXAMPLE_LCD_BK_LIGHT_ON_LEVEL);
+    delay(200);
+
+    Serial.println("Initialize Intel 8080 bus");
+    esp_lcd_i80_bus_handle_t i80_bus = NULL;
+    esp_lcd_i80_bus_config_t bus_config = {
+        .dc_gpio_num = EXAMPLE_PIN_NUM_DC,
+        .wr_gpio_num = EXAMPLE_PIN_NUM_PCLK,
+        .data_gpio_nums = {
+            EXAMPLE_PIN_NUM_DATA0,
+            EXAMPLE_PIN_NUM_DATA1,
+            EXAMPLE_PIN_NUM_DATA2,
+            EXAMPLE_PIN_NUM_DATA3,
+            EXAMPLE_PIN_NUM_DATA4,
+            EXAMPLE_PIN_NUM_DATA5,
+            EXAMPLE_PIN_NUM_DATA6,
+            EXAMPLE_PIN_NUM_DATA7,
+        },
+        .bus_width = 8,
+        .max_transfer_bytes = EXAMPLE_LCD_H_RES * EXAMPLE_LCD_V_RES * sizeof(uint16_t)
+    };
+    ESP_ERROR_CHECK(esp_lcd_new_i80_bus(&bus_config, &i80_bus));
+    esp_lcd_panel_io_handle_t io_handle = NULL;
+    esp_lcd_panel_io_i80_config_t io_config = {
+        .cs_gpio_num = EXAMPLE_PIN_NUM_CS,
+        .pclk_hz = EXAMPLE_LCD_PIXEL_CLOCK_HZ,
+        .trans_queue_depth = 10,
+        // .on_color_trans_done = example_notify_lvgl_flush_ready,
+        .user_ctx = &disp_drv,
+        .lcd_cmd_bits = EXAMPLE_LCD_CMD_BITS,
+        .lcd_param_bits = EXAMPLE_LCD_PARAM_BITS,
+        .dc_levels = {
+            .dc_idle_level = 0,
+            .dc_cmd_level = 0,
+            .dc_dummy_level = 0,
+            .dc_data_level = 1,
+        },
+        .flags = {
+            .swap_color_bytes = 1,
+        },
+    };
+    ESP_ERROR_CHECK(esp_lcd_new_panel_io_i80(i80_bus, &io_config, &io_handle));
+
+    Serial.println("Install LCD driver of st7789");
+    esp_lcd_panel_handle_t panel_handle = NULL;
+    esp_lcd_panel_dev_config_t panel_config = {
+        .reset_gpio_num = EXAMPLE_PIN_NUM_RST,
+        .color_space = ESP_LCD_COLOR_SPACE_RGB,
+        .bits_per_pixel = 16,
+    };
+    ESP_ERROR_CHECK(esp_lcd_new_panel_st7789(io_handle, &panel_config, &panel_handle));
+
+    // Send all the commands
+    for (int i = 0; i < sizeof(st_init_cmds) / sizeof(lcd_init_cmd_t); i ++)
+    {
+        esp_lcd_panel_io_tx_param(io_handle, st_init_cmds[i].cmd, st_init_cmds[i].data, st_init_cmds[i].len);
+        if (st_init_cmds[i].cmd & 0x11) {
+            delay(st_init_cmds[i].delay);
+        }
+    }
+
+    esp_lcd_panel_draw_bitmap(panel_handle, 0, 0, EXAMPLE_LCD_H_RES, EXAMPLE_LCD_V_RES, gImage_logo);
+    delay(3000);
+
+    lv_init();
+    // alloc draw buffers used by LVGL from PSRAM
+    lv_color_t *buf1 = (lv_color_t *)heap_caps_malloc(EXAMPLE_LCD_H_RES * EXAMPLE_LCD_V_RES * sizeof(lv_color_t), MALLOC_CAP_SPIRAM);
+    assert(buf1);
+    lv_color_t *buf2 = (lv_color_t *)heap_caps_malloc(EXAMPLE_LCD_H_RES * EXAMPLE_LCD_V_RES * sizeof(lv_color_t), MALLOC_CAP_SPIRAM);
+    assert(buf2);
+    lv_disp_draw_buf_init(&disp_buf, buf1, buf2, EXAMPLE_LCD_H_RES * EXAMPLE_LCD_V_RES);
+
+    Serial.println("Register display driver to LVGL");
+    lv_disp_drv_init(&disp_drv);
+    disp_drv.hor_res = EXAMPLE_LCD_H_RES;
+    disp_drv.ver_res = EXAMPLE_LCD_V_RES;
+    disp_drv.flush_cb = example_lvgl_flush_cb;
+    disp_drv.draw_buf = &disp_buf;
+    disp_drv.user_data = panel_handle;
+    lv_disp_t *disp = lv_disp_drv_register(&disp_drv);
+
+    // lv_indev_drv_init(&indev_drv);
+    // indev_drv.type = LV_INDEV_TYPE_POINTER;
+    // indev_drv.read_cb = lv_touchpad_read;
+    // lv_indev_drv_register(&indev_drv);
+
+    lv_demo_benchmark();
+}
+
+
+void loop() {
+    delay(2);
+    lv_timer_handler();
+}
